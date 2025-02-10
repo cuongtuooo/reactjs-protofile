f7ee0a6f5ab3d7be3a4856bc161f2b7489611451
cuongtuooo <cuonganh1004cva@gmail.com>
2025-02-10 19:00:40 +0700

    video 40

--- src/components/sections/resize.button.tsx
+++ src/components/sections/resize.button.tsx
@@ -3,13 +3,14 @@ interface IProp {
     btnText?: string;
     btnIcons?: React.ReactNode;
     btnStyle?: React.CSSProperties;
+    onClick?: () => void;
 }
 
 const ResizeButton = (props: IProp) => {
-    const { btnText, btnIcons, btnStyle } = props;
+    const { btnText, btnIcons, btnStyle, onClick } = props;
 
     return (
-        <button className="resize-button" style={btnStyle}>
+        <button onClick={onClick} className="resize-button" style={btnStyle}>
             <span style={{ textTransform: "uppercase" }}>{btnText}</span>
             <>{btnIcons}</>
         </button>
